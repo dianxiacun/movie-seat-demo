68ac89f0324cc1c3c992581eaf9c66c62549de74
awesome <awesome@computerituicndeMac-mini.local>
2016-05-06 01:01:41 +0800

    toast bug fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Seat from './components/Seat.vue'
+import Seat from './components/Seat.vue'
 // import CinemaSchedule from './components/CinemaSchedule.vue'
 import City from './components/City.vue'
 import CityDemo from './components/CityDemo.vue'
@@ -10,7 +10,7 @@ import ConfirmOrder from './components/ConfirmOrder.vue'
 import Payment from './components/Payment.vue'
 // import './assets/js/iSlider-animate.js'
 
-// Vue.component('seat', Seat)
+Vue.component('seat', Seat)
 // Vue.component('vue-toast', VueToast)
 // Vue.component('cinema-schedule', CinemaSchedule)
 Vue.component('city', City)
@@ -21,6 +21,6 @@ Vue.component('confirm-order', ConfirmOrder)
 Vue.component('payment', Payment)
 new Vue({
   el: 'body',
-  components: { City, CityDemo, MovieDetail, Movie, ConfirmOrder, Payment }
+  components: { Seat, City, CityDemo, MovieDetail, Movie, ConfirmOrder, Payment }
 })
 
